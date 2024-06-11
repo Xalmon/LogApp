f24e6469e4599d4996a83a3c62d09b2c1fcce2c8
Xalmon <xalmonoyefule@gmail.com>
2024-06-11 06:36:08 +0100

    first commit

---
+++ src/test/java/com/example/logapp/LogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
